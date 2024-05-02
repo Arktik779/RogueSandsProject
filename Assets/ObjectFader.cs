a4308b654faa5fa09909b4d7f59a6a1c2cca5c98
egorhoziaev <arktik779@gmail.com>
2024-05-02 17:53:40 +0300

    Created EasyRoom Greybox

---
+++ Assets/ObjectFader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFader : MonoBehaviour
+{
+    public float fadeSpeed, fadeAmount;
+    float originalOpacity;
+
+    Renderer renderer;
+    Material Mat;
+    public bool DoFade = false;
+
+    private void Start()
+    {
+        Mat = GetComponent<Renderer>().material;
+        originalOpacity = Mat.color.a;
+    }
+
+    private void Update()
+    {
+        if (DoFade)
+            FadeNow();
+        else
+        ResetFade();
+    }
+    void FadeNow()
+    {
+        Color currentColor = Mat.color;
+        Color smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b, Mathf.Lerp(currentColor.a,fadeAmount,fadeSpeed * Time.deltaTime));
+        Mat.color = smoothColor;
+    }
+
+    private void ResetFade()
+    {
+        Color currentColor = Mat.color;
+        Color smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b, Mathf.Lerp(currentColor.a, originalOpacity, fadeSpeed * Time.deltaTime));
+        Mat.color = smoothColor;
+    }
+}
